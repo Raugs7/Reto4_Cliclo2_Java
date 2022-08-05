0cf51e3028b56f27f878998e9df221f4e7349c45
Laura_Skaphe <lauragonsan7@gmail.com>
2022-08-04 19:59:46 -0500

    Más clases según estructura archivo

---
+++ RETO4/src/main/java/rau/reto4/Controlador/ReportesController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rau.reto4.Controlador;
+
+/**
+ *
+ * @author SKAPHE
+ */
+public class ReportesController {
+    
+}
