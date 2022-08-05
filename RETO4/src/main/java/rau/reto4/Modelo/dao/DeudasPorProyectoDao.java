978fbe0bca4ed1770a4abf136872e299b10e921b
David Luna <lic.dlgluna@iegp.edu.co>
2022-08-04 21:15:59 -0500

    Creacion de las clases Dao - DeudasPorProyecto y ProyectoBanco

---
+++ RETO4/src/main/java/rau/reto4/Modelo/dao/DeudasPorProyectoDao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rau.reto4.Modelo.dao;
+
+/**
+ *
+ * @author user
+ */
+public class DeudasPorProyectoDao {
+    
+}
